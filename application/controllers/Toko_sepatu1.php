2b309a34a4cdc6c915011c951526a1df2c0af59f
KikiIskandar12200500 <kikiiskandar2001@gmail.com>
2021-10-31 15:14:15 +0700

    TOKO SEPATU NEW

---
+++ application/controllers/Toko_sepatu1.php
@@ -0,0 +1,34 @@
+<?php
+class Toko_sepatu1 extends CI_Controller
+{
+    public function index()
+    {$this->load->view('form_toko_sepatu1' );}
+
+    public function pesan()
+    {
+        $this->form_validation->set_rules('nama' , 'Nama Pemesan' , 'required' ,
+            ['required' => 'Nama Pemesan Harus diisi']);
+
+        $this->form_validation->set_rules('telepon' , 'No. Telepon' , 'required|numeric' ,
+            ['required' => 'No. Telepon Harus diisi' ,
+             'numeric' => 'No. Telepon berupa angka']);
+
+        $this->form_validation->set_rules('merk' , 'Merk Sepatu' , 'required' ,
+             ['required' => 'Merk Sepatu harus dipilih']);
+
+        $this->form_validation->set_rules('ukuran' , 'Ukuran Sepatu' , 'required' ,
+             ['required' => 'Ukuran Sepatu harus dipilih']);
+ 
+        if ($this->form_validation->run() !=true){$this->load->view('form_toko_sepatu1');}
+        else
+        {
+            $data = [
+            'nama' => $this->input->POST('nama'),
+            'telepon' => $this->input->POST('telepon'),
+            'merk' => $this->input->POST('merk'),
+            'ukuran' => $this->input->POST('ukuran')];
+
+            $this->load->view('Data_toko_sepatu1' , $data);
+        }
+    }
+}
